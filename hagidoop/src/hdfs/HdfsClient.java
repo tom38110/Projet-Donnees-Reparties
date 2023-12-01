55c5df34c2a015fdcbab4f19d3cdaa01959263df
yguirene <120386159+yguirene@users.noreply.github.com>
2023-12-01 09:48:31 +0100

    mise à jour du HdfsClient
    Implémentation de delete et mise en place de write et read (en cours).

--- hagidoop/src/hdfs/HdfsClient.java
+++ hagidoop/src/hdfs/HdfsClient.java
@@ -1,4 +1,6 @@
 package hdfs;
+import java.io.File;
+import java.io.*;
 
 public class HdfsClient {
 	
@@ -9,16 +11,43 @@ public class HdfsClient {
 	}
 	
 	public static void HdfsDelete(String fname) {
+		try {
+		File fichier = new File("/mnt/c/Users/yanis/Documents/GitHub/Projet-Donnees-Reparties/" 
+								+ fname); // modifier le path avec celui de Hagimont
+		if (fichier.delete()) {
+			system.out.println("le fichier : " + fname + " a été effacé.");
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
+	// fmt format du fichier (FMT_TXT ou FMT_KV)
+	// lis chaque ligne du fichier sans le découper et stock les fragments dans les différents noeuds
 	public static void HdfsWrite(int fmt, String fname) {
+		File fichier = new File("/mnt/c/Users/yanis/Documents/GitHub/Projet-Donnees-Reparties/" 
+								+ fname); // modifier le path avec celui de Hagimont
+		FileReader lectFich = new FileReader(fichier);
+		BufferedReader bufLect = new BufferedReader(lectFich);
+		
+		if (fmt == 0) { // FMT_TXT	
+		
+		} else if (fmt == 1) {// FMT_KV
+
+		} else {
+
+		}
 	}
 
 	public static void HdfsRead(String fname) {
+		File fichier = new File("/mnt/c/Users/yanis/Documents/GitHub/Projet-Donnees-Reparties/" 
+								+ fname); // modifier le path avec celui de Hagimont
+		
 	}
 
 	public static void main(String[] args) {
 		// java HdfsClient <read|write> <txt|kv> <file>
 		// appel des méthodes précédentes depuis la ligne de commande
+
 	}
 }
