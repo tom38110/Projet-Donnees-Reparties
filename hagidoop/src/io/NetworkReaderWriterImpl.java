b84fc65cdb43360baafa44ee26ed175a83ca94a3
YoussefELALAMI <youssef2003elalami@gmail.com>
2024-01-11 09:40:58 +0100

    NetworkReaderWriter

--- hagidoop/src/io/NetworkReaderWriterImpl.java
+++ hagidoop/src/io/NetworkReaderWriterImpl.java
@@ -9,16 +9,30 @@ import interfaces.NetworkReaderWriter;
 
 public class NetworkReaderWriterImpl implements NetworkReaderWriter {
 
-    private BlockingQueue<KV> queue; // Pour gérer la communication entre les Map et le Reducer
     private Socket clientSocket;
     private ServerSocket serverSocket;
     private BufferedReader reader;
     private BufferedWriter writer;
+    private String host;
+    private int port;
 
-    public NetworkReaderWriterImpl() {
-        this.queue = new LinkedBlockingQueue<>();
+
+    /**
+     * Constructeur pour le NetworkReaderWriterImpl
+     * @param port
+     * @param host
+     */
+    public NetworkReaderWriterImpl(String host, int port) {
+        this.host =host;
+        this.port = port;
     }
 
+    /**
+     * Méthode pour envoyer un fragment au Reduce
+     * @param clientSocket
+     * @param fragmentFile
+     * @throws IOException
+     */
     @Override
     public KV read() {
         try {
@@ -33,97 +47,78 @@ public class NetworkReaderWriterImpl implements NetworkReaderWriter {
         return null;
     }
 
+    /**
+     * Méthode pour écrire KV record
+     * @param record
+     * @throws IOException
+     */
     @Override
     public void write(KV record) {
         try {
-            queue.put(record);
+            //queue.put(record);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Méthode pour ouvrir le serveur (Reduce)
+     */
     @Override
     public void openServer() {
         try {
-            serverSocket = new ServerSocket(); // *********** Je sais pas quel port je dois mettre
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void openClient() {
-        try {
-            clientSocket = new Socket();// *********** Je sais pas quel port je dois mettre
-            writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            serverSocket = new ServerSocket(port);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * Méthode pour accepter une connexion depuis un Map
+     * Méthode pour ouvrir le client (Map)
+     */
+    @Override
+    public void openClient() {
+        try {
+            clientSocket = new Socket(host, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Méthode pour accepter la connexion
+     * @return
      */
     @Override
     public NetworkReaderWriter accept() {
         try {
             clientSocket = serverSocket.accept(); // Le Reduce attend qu'un Map se connecte 
-            writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())); // Pour écrire dans le Reduce
-            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Pour lire les données qui viennent depuis les Maps
-
-            // Un thread pour gérer l'écriture dans le Reduce
-            Thread reducerWriterThread = new Thread(() -> {
-                try {
-                    while (true) {
-                        KV record = queue.take(); // Récupération les données depuis la queue
-                        if (record == null) {
-                            break; // Fin d'écriture
-                        }
-                        // Ecrire dans le reduce
-                        writer.write(record.k + KV.SEPARATOR + record.v); // Ecrire la donnée qui vient depuis la queue sous forme d'un KV
-                        writer.newLine(); // Retourne à la ligne
-                        writer.flush(); // Pour s'assurer que toutes les données sont écrites immédiatement dans la sortie, comme ça on n'attend pas que le tampon (le buffer) se remplit avant d'envoyé
-                    }
-                } catch (InterruptedException | IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        // Fermeture de la connexion
-                        writer.close();
-                        reader.close();
-                        clientSocket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            // Lancer le Thread
-            reducerWriterThread.start();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
         return this;
     }
 
+    /**
+     * Méthode pour fermer le serveur
+     */
     @Override
     public void closeServer() {
         try {
-            // Marquer la fin de l'écriture en ajoutant null à la fin de la queue
-            queue.put(null);
+            //queue.put(null);
             serverSocket.close();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Méthode pour fermer le client
+     */
     @Override
     public void closeClient() {
         try {
             clientSocket.close();
-            writer.close();
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
