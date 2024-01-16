896063237ab6b188cc057a977590f7534528d3fd
tom38110 <tom.audard@gmail.com>
2024-01-16 08:27:56 +0100

    project

--- hagidoop/src/config/Project.java
+++ hagidoop/src/config/Project.java
@@ -5,10 +5,10 @@ public class Project {
 	//public static String PATH = "C:/Users/UserPC/Documents/GitHub/Projet-Donnees-Reparties/hagidoop/"; // Yanis
 	// public static String PATH = "/home/yelalami/Bureau/GitHub/Projet-Donnees-Reparties/hagidoop/"; //  Youssef
 	public static String PATH = "/home/tom/Documents/Projet-Donnees-Reparties/hagidoop/"; // Tom
-	public static int nbNoeud = 6;
-	public static String hosts[] = {"localhost", "localhost", "localhost", "localhost", "localhost", "localhost"};
-	public static int ports[] = {8081, 8082, 8083, 8084, 8085, 8086};
-	public static int portWorker = 8087;
+	public static int nbNoeud = 3;
+	public static String hosts[] = {"taudard@balrog", "taudard@taupiqueur", "taudard@salameche"};
+	public static int ports[] = {8100, 8101, 8102};
+	public static int portWorker = 8103;
 	public static String hostInit = "localhost";
 	public static int portInit = 8080;
 }
